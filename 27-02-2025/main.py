7fff6d46bdf673dc29e47387a665a4cab111e795
Marco <126587897+MarCalll@users.noreply.github.com>
2025-02-27 17:19:00 +0100

    Update main.py

--- 27-02-2025/main.py
+++ 27-02-2025/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.datasets import load_wine
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 #Branch1
 pinguini = sns.load_dataset('penguins')
@@ -20,6 +21,23 @@ islandCount = pinguini['island'].value_counts()
 print("\nbody mass by sex:\n",mean_body_mass_g_sex)
 print("\nflipper length by sex:\n",flipper_length_mm_sex)
 print("\nisland count:\n",islandCount)
+
+#Branch3
+correlation_matrix = pinguini.select_dtypes(include=["number"]).corr()
+correlation_matrix
+plt.figure(figsize=(10,6))
+sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
+plt.show()
+
+plt.figure(figsize=(8, 6))
+sns.scatterplot(x='body_mass_g', y='flipper_length_mm', hue='species', data=pinguini, palette='Set2')
+plt.title('Relazione tra peso corporeo e lunghezza delle pinne dei pinguini')
+plt.xlabel('Peso corporeo (g)')
+plt.ylabel('Lunghezza pinne (mm)')
+plt.legend(title='Specie')
+plt.show()
+
+
 
 
 
