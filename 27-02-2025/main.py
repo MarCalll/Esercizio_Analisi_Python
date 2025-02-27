5495772337a2db049d9b97e4a0d001d34adaedb4
Marco <126587897+MarCalll@users.noreply.github.com>
2025-02-27 17:12:05 +0100

    Update main.py

--- 27-02-2025/main.py
+++ 27-02-2025/main.py
@@ -5,8 +5,22 @@ import seaborn as sns
 #Branch1
 pinguini = sns.load_dataset('penguins')
 print(pinguini)
+pinguini.dropna(inplace=True)
 print(pinguini.info())
-print(pinguini.describe())
+# print(pinguini.describe())
 
-specie = pinguini['species'].value_counts()
-print("\nQuantità specie:\n",specie)
+specieCount = pinguini['species'].value_counts()
+print("\nQuantità specie:\n",specieCount)
+
+#Branch2
+# studiare la relazione tra peso corporeo dei pinguini con altre variabili come sesso e lunghezza del becco
+mean_body_mass_g_sex = pinguini.groupby('sex')['body_mass_g'].mean()
+flipper_length_mm_sex = pinguini.groupby('sex')['flipper_length_mm'].mean()
+islandCount = pinguini['island'].value_counts()
+print("\nbody mass by sex:\n",mean_body_mass_g_sex)
+print("\nflipper length by sex:\n",flipper_length_mm_sex)
+print("\nisland count:\n",islandCount)
+
+
+
+
